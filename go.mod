5141e14dd2f9cfc1e38a381ca4e8aa224522620a
Marius Sofron <msofron@adobe.com>
2025-02-21 09:02:00 +0200

    added dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mariussofron/puppy
 
 go 1.24.0
+
+require github.com/mariussofron/big-dog v0.0.0-20250221065843-b66c6d5cf696
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	bigdog "github.com/mariussofron/big-dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof! Woof!"
+}
+
+func BigBark(s string) string {
+	return bigdog.BigBarks(Bark())
+}
+
+func BigBarks(s string) string {
+	return bigdog.BigBarks(Barks())
 }
